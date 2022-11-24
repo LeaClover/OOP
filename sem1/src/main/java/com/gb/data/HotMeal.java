72137316436d2daa71da23a4a49b7d73e6db6a60
LeaClover <plesen_14@mail.ru>
2022-11-24 11:55:19 +0300

    add new project hot meal

---
+++ sem1/src/main/java/com/gb/data/HotMeal.java
@@ -0,0 +1,24 @@
+﻿package com.gb.data;
+
+public class HotMeal extends Product{
+
+    String type;
+
+    public HotMeal(String name, Double cost, String type) {
+        super(name, cost);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "HotMeal [name=" + super.getName() + ", cost=" + super.getCost() + ", type=" + type + "]";
+    }
+}
