615b0320a21e7e028316da6513b1215f483a709f
LeaClover <plesen_14@mail.ru>
2022-11-24 11:55:49 +0300

    add new project machine

---
+++ sem1/src/main/java/com/gb/data/Machine.java
@@ -0,0 +1,21 @@
+﻿package com.gb.data;
+
+import java.util.List;
+
+public class Machine {
+    private final List<Product> list;
+
+    public Machine(List<Product> list) {
+        this.list = list;
+    }
+
+    public Product getProductByName(String name) {
+        
+        for (Product item: this.list) {
+            if (name.equals(item.getName())) {
+                return item;
+            }
+        }
+        throw new IllegalStateException("Product not found!");
+    }
+}
