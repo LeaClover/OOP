bdb0c6c3506c6446b0b4e0ac683fd5c6ad79a9bc
LeaClover <plesen_14@mail.ru>
2022-11-24 11:54:37 +0300

    add new project drinks

---
+++ sem1/src/main/java/com/gb/data/Drinks.java
@@ -0,0 +1,25 @@
+﻿package com.gb.data;
+
+public class Drinks extends Product {
+
+    Double volume;
+
+    public Drinks(String name, Double cost, Double volume) {
+        super(name, cost);
+        this.volume = volume;
+    }
+
+    public Double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Double volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Drinks [name=" + super.getName() + ", cost=" + super.getCost() + ", volume=" + volume + "]";
+    }
+    
+}
