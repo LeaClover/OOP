61c6524871afd4077625817fae924d438c2c3849
LeaClover <plesen_14@mail.ru>
2022-11-24 11:54:50 +0300

    add new project hot drinks

---
+++ sem1/src/main/java/com/gb/data/HotDrinks.java
@@ -0,0 +1,27 @@
+﻿package com.gb.data;
+
+public class HotDrinks extends Drinks{
+
+    int temp;
+
+    public HotDrinks(String name, Double cost, Double volume, int temp) {
+        super(name, cost, volume);
+        this.temp = temp;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    @Override
+    public String toString() {
+        return "HotDrinks [name=" + super.getName() + ", cost=" + super.getCost() + ", volume=" + volume + ", temp=" + temp + "]";
+    }
+
+    
+    
+}
