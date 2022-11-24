02c105c03ad1cb65edafedd73a83654d385aedec
LeaClover <plesen_14@mail.ru>
2022-11-24 11:56:12 +0300

    add new project snacks

---
+++ sem1/src/main/java/com/gb/data/Snacks.java
@@ -0,0 +1,24 @@
+﻿package com.gb.data;
+
+public class Snacks extends Product {
+
+    String taste;
+
+    public Snacks(String name, Double cost, String taste) {
+        super(name, cost);
+        this.taste = taste;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    @Override
+    public String toString() {
+        return "Snacks [name=" + super.getName() + ", cost=" + super.getCost() + ", taste=" + taste + "]";
+    }
+}
