9b554a4a1f2a9f2229a80d0edd0ecdbc78bcc596
LeaClover <plesen_14@mail.ru>
2022-11-24 11:55:33 +0300

    add new project hot meal machine

---
+++ sem1/src/main/java/com/gb/data/HotMealMachine.java
@@ -0,0 +1,11 @@
+﻿package com.gb.data;
+
+import java.util.List;
+
+public class HotMealMachine extends Machine{
+
+    public HotMealMachine(List<Product> list) {
+        super(list);
+    }
+    
+}
