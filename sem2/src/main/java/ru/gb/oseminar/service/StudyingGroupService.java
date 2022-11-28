533300190c622f3275877a1db00b6c35516f58d2
LeaClover <plesen_14@mail.ru>
2022-11-29 02:24:07 +0300

    add new project file stud gr ser

---
+++ sem2/src/main/java/ru/gb/oseminar/service/StudyingGroupService.java
@@ -0,0 +1,21 @@
+﻿package ru.gb.oseminar.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.data.Teacher;
+
+public class StudyingGroupService {
+
+    final Map<Teacher, List<Student>> studyingGroup;
+
+    public StudyingGroupService() {
+        this.studyingGroup = new HashMap<>();
+    }
+    
+    public Map<Teacher, List<Student>> createStudyingGroup(Teacher teacher, List<Student> students) {
+        this.studyingGroup.put(teacher, students);
+        return studyingGroup;
+    }
+}
