ae58372f86c2383602b03285c18254040da8e560
LeaClover <plesen_14@mail.ru>
2022-11-29 02:26:50 +0300

    add new project file teach view

---
+++ sem2/src/main/java/ru/gb/oseminar/view/TeacherView.java
@@ -0,0 +1,18 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.logging.Logger;
+import ru.gb.oseminar.data.Teacher;
+import ru.gb.oseminar.data.User;
+
+public class TeacherView {
+
+    public void sendOnController(List<User> users) {
+        Logger log = Logger.getAnonymousLogger();
+        for(User user: users) {
+            if(user instanceof Teacher) {
+                log.info(user.toString());
+            }
+        }
+    }
+}
