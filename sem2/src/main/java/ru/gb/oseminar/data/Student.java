3a8f0e0d84a72edba26ce1ce79bd966bbf7d98d6
LeaClover <plesen_14@mail.ru>
2022-11-29 02:26:11 +0300

    add new project file student

---
+++ sem2/src/main/java/ru/gb/oseminar/data/Student.java
@@ -0,0 +1,24 @@
+﻿package ru.gb.oseminar.data;
+
+public class Student extends User{
+
+    private Long studentID;
+
+    public Student(Long studentID, String firstName, String lastName, String patronymic) {
+        super(firstName, lastName, patronymic);
+        this.studentID = studentID;
+    }
+
+    public Long getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Long studentID) {
+        this.studentID = studentID;
+    }
+
+    @Override
+    public String toString() {
+        return studentID + " - " + super.toString();
+    }
+}
