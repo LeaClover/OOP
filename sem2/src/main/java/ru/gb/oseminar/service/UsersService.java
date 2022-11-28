8b6f43866c3a9baf0937b0d5af639f704c70793e
LeaClover <plesen_14@mail.ru>
2022-11-29 02:25:03 +0300

    add new project file user ser

---
+++ sem2/src/main/java/ru/gb/oseminar/service/UsersService.java
@@ -0,0 +1,46 @@
+﻿package ru.gb.oseminar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.data.User;
+
+public class UsersService implements DataService {
+
+    final List<User> users;
+    final List<Student> students;
+
+    public UsersService() {
+        this.users = new ArrayList<User>();
+        this.students = new ArrayList<Student>();
+    }
+
+    @Override
+    public void createUser(String firstName, String lastName, String patronymic) {
+        Long id = 0L;
+        for(User item: this.users) {
+            if(item instanceof Student) {
+                if(id < ((Student) item).getStudentID()) {
+                    id = ((Student) item).getStudentID();
+                }
+            }
+        }
+        this.users.add(new Student(++id, firstName, lastName, patronymic));
+    }
+
+    @Override
+    public List<User> getAll() {
+        return this.users;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        for(User item: users) {
+            if(item instanceof Student) {
+                if(id.equals(((Student) item).getStudentID())) {
+                    this.users.remove(item);
+                }
+            }
+        }
+    }
+}
