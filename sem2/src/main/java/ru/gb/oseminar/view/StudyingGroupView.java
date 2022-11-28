a7662f9394aa1f80a0293616e2946b4741dab13a
LeaClover <plesen_14@mail.ru>
2022-11-29 02:27:19 +0300

    add new project file stud group view

---
+++ sem2/src/main/java/ru/gb/oseminar/view/StudyingGroupView.java
@@ -0,0 +1,20 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.data.Teacher;
+
+public class StudyingGroupView {
+    
+    public void sendOnController(Map<Teacher, List<Student>>studyingGroup) {
+        Logger log = Logger.getAnonymousLogger();
+        for(Teacher teacher: studyingGroup.keySet()) {
+            log.info(String.format("Учебная группа №__, Преподаватель: %s. \n\nСписок студентов: \n", teacher.toString()));
+            for(Student student: studyingGroup.get(teacher)) {
+                log.info(String.format("%s", student.toString()));
+            }
+        }
+    }
+}
