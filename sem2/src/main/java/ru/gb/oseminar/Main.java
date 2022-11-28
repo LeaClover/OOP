e393d3db90d1eb38828811f8ef1473b486e781ad
LeaClover <plesen_14@mail.ru>
2022-11-29 02:23:26 +0300

    add new project file main

---
+++ sem2/src/main/java/ru/gb/oseminar/Main.java
@@ -0,0 +1,16 @@
+﻿package ru.gb.oseminar;
+
+import ru.gb.oseminar.controller.Controller;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Controller contr = new Controller();
+        contr.createTeacher("Ivan", "Ivanov", "Ivanovich");
+        contr.createUser("Olesya", "Rakhmanina", "Dmitrievna");
+        contr.createUser("Alex", "Alexanrov", "Igorevich");
+        contr.createUser("Egor", "Habarov", "Antonovich");
+        contr.getStudentsList();
+        contr.createStudyingGroup(contr.getTeacher(), contr.getStudentsList());
+    }   
+}
