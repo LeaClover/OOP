502abf0785944df3f1fac4fd0d0d44a10cfb02ae
LeaClover <plesen_14@mail.ru>
2022-11-29 02:26:23 +0300

    add new project file stud group

---
+++ sem2/src/main/java/ru/gb/oseminar/data/StudyingGroup.java
@@ -0,0 +1,31 @@
+﻿package ru.gb.oseminar.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class StudyingGroup {
+    
+    private Teacher teacher;
+    private List<Student> studentsList = new ArrayList<>();
+    
+    public StudyingGroup(Teacher teacher, List<Student> studentsList) {
+        this.teacher = teacher;
+        this.studentsList = studentsList;
+    }
+
+    public User getTeachUser() {
+        return teacher;
+    }
+
+    public void setTeachUser(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudentsList() {
+        return studentsList;
+    }
+
+    public void setStudentsList(List<Student> studentsList) {
+        this.studentsList = studentsList;
+    }
+}
