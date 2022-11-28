07cfe3ba866fc76f8e2ea40561a2ecff41db1fa8
LeaClover <plesen_14@mail.ru>
2022-11-29 02:25:43 +0300

    add new project file teacher

---
+++ sem2/src/main/java/ru/gb/oseminar/data/Teacher.java
@@ -0,0 +1,24 @@
+﻿package ru.gb.oseminar.data;
+
+public class Teacher extends User{
+
+    private Long teacherID;
+
+    public Teacher(Long teacherID, String firstName, String lastName, String patronymic) {
+        super(firstName, lastName, patronymic);
+        this.teacherID = teacherID;
+    }
+
+    public Long getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(Long teacherID) {
+        this.teacherID = teacherID;
+    }
+
+    @Override
+    public String toString() {
+        return teacherID + " - " + super.toString();
+    }
+}
