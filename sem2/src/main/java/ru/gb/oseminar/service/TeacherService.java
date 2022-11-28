6a993ce270b890adc6cf5612eb92549d2d8379e7
LeaClover <plesen_14@mail.ru>
2022-11-29 02:24:45 +0300

    add new project file teach ser

---
+++ sem2/src/main/java/ru/gb/oseminar/service/TeacherService.java
@@ -0,0 +1,36 @@
+﻿package ru.gb.oseminar.service;
+
+import ru.gb.oseminar.data.Teacher;
+import ru.gb.oseminar.data.User;
+
+public class TeacherService extends UsersService{
+
+    private Teacher teacher;
+    public TeacherService() {
+    }
+
+    public void createTeacher(String firstName, String lastName, String patronymic) {
+        super.createUser(firstName, lastName, patronymic);
+        Long id = 0L;
+        for(User item: this.users) {
+            if(item instanceof Teacher) {
+                if(id < ((Teacher) item).getTeacherID()) {
+                    id = ((Teacher) item).getTeacherID();
+                }
+            }
+        }
+        this.teacher = new Teacher(++id, firstName, lastName, patronymic);
+        this.users.add(this.teacher);
+    }
+
+    public void deleteTeacher(Long id) {
+        super.deleteUser(id);
+        for(User item: users) {
+            if(item instanceof Teacher) {
+                if(id.equals(((Teacher) item).getTeacherID())) {
+                    this.users.remove(item);
+                }
+            }
+        }
+    }
+}
