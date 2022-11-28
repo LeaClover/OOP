26f471fa0555f70a165d9948cb62eac81a6f44ef
LeaClover <plesen_14@mail.ru>
2022-11-29 02:27:02 +0300

    add new project file user view

---
+++ sem2/src/main/java/ru/gb/oseminar/view/UserView.java
@@ -0,0 +1,15 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.logging.Logger;
+import ru.gb.oseminar.data.User;
+
+public class UserView {
+
+    public void sendOnController(List<User> users) {
+        Logger log = Logger.getAnonymousLogger();
+        for(User user: users) {
+            log.info(user.toString());
+        }
+    }
+}
