ac2dcbb6d3a9ca8604a537f00a4828a0e6ce636e
LeaClover <plesen_14@mail.ru>
2022-12-02 20:51:16 +0300

    add studying group comparator file

---
+++ sem3/src/main/java/ru/gb/oseminar/data/StudyingGroupComparator.java
@@ -0,0 +1,14 @@
+﻿package ru.gb.oseminar.data;
+
+import java.util.Comparator;
+
+public class StudyingGroupComparator implements Comparator <Student> {
+
+    @Override
+    public int compare(Student s1, Student s2) {
+        if (s1.getLastName().equalsIgnoreCase(s2.getLastName())) {
+            return s1.getFirstName().compareTo(s2.getFirstName());
+        }
+        return s1.getLastName().compareTo(s2.getLastName());
+    }
+}
