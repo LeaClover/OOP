4bf3474d4b584d09bb0a30bb72ab8281f7be88d0
LeaClover <plesen_14@mail.ru>
2022-12-02 20:49:47 +0300

    add main file

---
+++ sem3/src/main/java/ru/gb/oseminar/Main.java
@@ -0,0 +1,30 @@
+﻿package ru.gb.oseminar;
+
+import ru.gb.oseminar.controller.Controller;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Controller contr = new Controller();
+        contr.createTeacher("Ivan", "Ivanov", "Ivanovich");
+        contr.createUser("Olesya", "Rakhmanina", "Dmitrievna");
+        contr.createUser("Alex", "Alexanrov", "Igorevich");
+        contr.createUser("Egor", "Habarov", "Antonovich");
+        contr.createStudyingGroups();
+        
+        contr.createTeacher("Petr", "Petrov", "Petrovich");
+        contr.createUser("Karina", "Rakhmanina", "Dmitrievna");
+        contr.createUser("Evgeniy", "Alexanrov", "Igorevich");
+        contr.createUser("Anna", "Habarova", "Antonovna");
+        contr.createStudyingGroups();
+        
+        contr.createTeacher("Semen", "Semenov", "Semenovich");
+        contr.createUser("Anna", "Rakhmanina", "Dmitrievna");
+        contr.createUser("Kristina", "Alexanrova", "Evgenevna");
+        contr.createUser("Vladislav", "Rakhmanin", "Aleksandrov");
+        contr.createStudyingGroups();
+
+        contr.showStudyingGroup(contr.getStudyingGroup());
+        contr.showSortStudyingGroup(contr.getStudyingGroup());
+    }   
+}
