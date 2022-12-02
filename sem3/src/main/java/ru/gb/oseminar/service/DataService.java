b031b184b944a0d5ef0890fb5dcf04b139c21cfd
LeaClover <plesen_14@mail.ru>
2022-12-02 20:52:20 +0300

    add data service file

---
+++ sem3/src/main/java/ru/gb/oseminar/service/DataService.java
@@ -0,0 +1,11 @@
+﻿package ru.gb.oseminar.service;
+
+import java.util.List;
+import ru.gb.oseminar.data.User;
+
+public interface DataService {
+
+    void createUser(String firstName, String lastName, String patronymic);
+    List<User> getAll();
+    void deleteUser(Long id);
+}
