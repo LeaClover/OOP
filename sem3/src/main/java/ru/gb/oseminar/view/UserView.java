dddb3f5db4e6d44128c0f3dddc7dad8895c0e312
LeaClover <plesen_14@mail.ru>
2022-12-02 20:53:44 +0300

    add user view file

---
+++ sem3/src/main/java/ru/gb/oseminar/view/UserView.java
@@ -0,0 +1,31 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.data.Teacher;
+import ru.gb.oseminar.data.User;
+
+public class UserView {
+
+    public void sendOnController(List<User> users) {
+        Logger log = Logger.getAnonymousLogger();
+        for(User user: users) {
+            if(user instanceof Teacher) {
+                log.info("Преподаватель" + user.toString());
+            }
+            if(user instanceof Student) {
+                log.info("Студент" + user.toString());
+            }
+        }
+    }
+
+    public void sendOnControllerSTD(List<Student> stud) {
+        Logger log = Logger.getAnonymousLogger();
+        log.info("\nСписок студентов:\n");
+        for(Student st: stud) {
+            log.info(st.toString());
+        }
+    }
+}
