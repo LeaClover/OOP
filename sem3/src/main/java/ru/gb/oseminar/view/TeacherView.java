3a8fac4aa030482c7167eea93c1a9ba64137f6f6
LeaClover <plesen_14@mail.ru>
2022-12-02 20:53:31 +0300

    add teacher view file

---
+++ sem3/src/main/java/ru/gb/oseminar/view/TeacherView.java
@@ -0,0 +1,15 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.logging.Logger;
+import ru.gb.oseminar.data.Teacher;
+
+public class TeacherView {
+
+    public void sendOnController(List<Teacher> teachers) {
+        Logger log = Logger.getAnonymousLogger();
+        for(Teacher item: teachers) {
+            log.info(item.toString());
+        }
+    }
+}
