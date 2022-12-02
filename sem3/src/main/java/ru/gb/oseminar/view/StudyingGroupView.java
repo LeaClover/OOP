673a1a7d8d250a9c7bd04f44f784ac8064477bfd
LeaClover <plesen_14@mail.ru>
2022-12-02 20:53:17 +0300

    add stud group view file

---
+++ sem3/src/main/java/ru/gb/oseminar/view/StudyingGroupView.java
@@ -0,0 +1,19 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.logging.Logger;
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.data.StudyingGroup;
+
+public class StudyingGroupView {
+    
+    public void sendOnController(List<StudyingGroup> studyingGroup) {
+        Logger log = Logger.getAnonymousLogger();
+        for(StudyingGroup studGroup: studyingGroup) {
+            log.info(studGroup.toString());
+            for(Student item: studGroup.getStudentsList()) {
+                log.info(item.toString());
+            }
+        }
+    }
+}
