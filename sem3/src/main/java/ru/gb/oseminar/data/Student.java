61d144d924be26beddd1bbfeadb0c1ba6e733d14
LeaClover <plesen_14@mail.ru>
2022-12-02 20:50:40 +0300

    add student file

---
+++ sem3/src/main/java/ru/gb/oseminar/data/Student.java
@@ -0,0 +1,34 @@
+﻿package ru.gb.oseminar.data;
+
+public class Student extends User{
+
+    private Long studentID;
+    private Long groupID;
+
+    public Student(Long studentID, String firstName, String lastName, String patronymic, Long groupID) {
+        super(firstName, lastName, patronymic);
+        this.studentID = studentID;
+        this.groupID = groupID;
+    }
+
+    public Long getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Long studentID) {
+        this.studentID = studentID;
+    }
+
+    public Long getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(Long groupID) {
+        this.groupID = groupID;
+    }
+
+    @Override
+    public String toString() {
+        return studentID + " - " + super.toString() + " - Группа - " + groupID;
+    }
+}
