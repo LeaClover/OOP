ecb918edad06b73840a35829352ebaae1a455df5
LeaClover <plesen_14@mail.ru>
2022-12-07 18:32:04 +0300

    add todo imp service

---
+++ sem4/src/main/java/ru/gb/oseminar/service/ToDoImportService.java
@@ -0,0 +1,22 @@
+﻿package ru.gb.oseminar.service;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ToDoImportService {
+
+    public List<String> importFromCSV() throws Exception {
+        List<String> list = new ArrayList<>();
+        FileReader file = new FileReader("C:/OOP/sem4/src/main/java/ru/gb/oseminar/text/ToDoListLOAD.csv");
+        Scanner sc = new Scanner(file);
+        file.read();
+        while(sc.hasNextLine()) {
+            list.add(sc.nextLine());
+        }
+        file.close();
+        sc.close();
+        return list;
+    }
+}
