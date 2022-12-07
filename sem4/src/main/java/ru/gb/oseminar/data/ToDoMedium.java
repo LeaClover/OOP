c0e3d8ba1d75283334cc08466b5b73c07f3a37c9
LeaClover <plesen_14@mail.ru>
2022-12-07 18:30:43 +0300

    add todo med

---
+++ sem4/src/main/java/ru/gb/oseminar/data/ToDoMedium.java
@@ -0,0 +1,31 @@
+﻿package ru.gb.oseminar.data;
+
+public class ToDoMedium extends ToDo{
+
+    protected final String statusMedium;
+    protected Long mediumID;
+    
+    public ToDoMedium(Long mediumID, String toDoDate, String toDoTime, String toDoDateDeadLine, String toDoTimeDeadLine,
+    String toDoAuthor, String toDoName) {
+        super(toDoDate, toDoTime, toDoDateDeadLine, toDoTimeDeadLine, toDoAuthor, toDoName);
+        this.mediumID = mediumID;
+        this.statusMedium = "MEDIUM";
+    }
+
+    public String getStatusMedium() {
+        return statusMedium;
+    }
+
+    public Long getMediumID() {
+        return mediumID;
+    }
+
+    public void setMediumID(Long mediumID) {
+        this.mediumID = mediumID;
+    }
+
+    @Override
+    public String toString() {
+        return "ID(" + mediumID + ") - " + statusMedium + " - " + super.toString();
+    }
+}
