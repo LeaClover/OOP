8dfd58d38eb2eea70994339a9047db9a4b89f8dc
LeaClover <plesen_14@mail.ru>
2022-12-07 18:30:11 +0300

    add todo comparator

---
+++ sem4/src/main/java/ru/gb/oseminar/data/ToDoComparator.java
@@ -0,0 +1,23 @@
+﻿package ru.gb.oseminar.data;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+
+public class ToDoComparator implements Comparator<ToDo> {
+    
+    @Override
+    public int compare(ToDo toDo1, ToDo toDo2) {
+        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+        DateFormat tm = new SimpleDateFormat("hh:mm");
+        try {
+            if(df.parse(toDo1.getToDoDateDeadLine()).equals(df.parse(toDo2.getToDoDateDeadLine()))) {
+                return tm.parse(toDo1.getToDoTimeDeadLine()).compareTo(tm.parse(toDo2.getToDoTimeDeadLine()));
+            }
+            return df.parse(toDo1.getToDoDateDeadLine()).compareTo(df.parse(toDo2.getToDoDateDeadLine()));
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
