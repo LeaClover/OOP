48f0a001bebe52426f51741f63a6e17d5414162a
LeaClover <plesen_14@mail.ru>
2022-12-07 18:30:52 +0300

    add todo urgent

---
+++ sem4/src/main/java/ru/gb/oseminar/data/ToDoUrgent.java
@@ -0,0 +1,31 @@
+﻿package ru.gb.oseminar.data;
+
+public class ToDoUrgent extends ToDo{
+
+    protected final String statusUrgent;
+    protected Long urgentID;
+
+    public ToDoUrgent(Long urgentID, String toDoDate, String toDoTime, String toDoDateDeadLine, String toDoTimeDeadLine,
+    String toDoAuthor, String toDoName) {
+        super(toDoDate, toDoTime, toDoDateDeadLine, toDoTimeDeadLine, toDoAuthor, toDoName);
+        this.urgentID = urgentID;
+        this.statusUrgent = "ALARM";
+    }
+
+    public String getStatusUrgent() {
+        return statusUrgent;
+    }
+
+    public Long getUrgentID() {
+        return urgentID;
+    }
+
+    public void setUrgentID(Long urgentID) {
+        this.urgentID = urgentID;
+    }
+
+    @Override
+    public String toString() {
+        return "ID(" + urgentID + ") - " + statusUrgent + " - " + super.toString();
+    }
+}
