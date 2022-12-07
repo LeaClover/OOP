267f4e83252903927bdb9111acea3b0e517e7931
LeaClover <plesen_14@mail.ru>
2022-12-07 18:30:25 +0300

    add todo group

---
+++ sem4/src/main/java/ru/gb/oseminar/data/ToDoGroup.java
@@ -0,0 +1,46 @@
+﻿package ru.gb.oseminar.data;
+
+import java.util.List;
+
+public class ToDoGroup {
+
+    private List<ToDoLow> toDoLowLis;
+    private List<ToDoMedium> toDoMedLis;
+    private List<ToDoUrgent> toDoUrgLis;
+
+    public ToDoGroup(List<ToDoLow> toDoLowLis, List<ToDoMedium> toDoMedLis, List<ToDoUrgent> toDoUrgLis) {
+        this.toDoLowLis = toDoLowLis;
+        this.toDoMedLis = toDoMedLis;
+        this.toDoUrgLis = toDoUrgLis;
+    }
+
+    public List<ToDoLow> getToDoLowLis() {
+        return toDoLowLis;
+    }
+
+    public void setToDoLowLis(List<ToDoLow> toDoLowLis) {
+        this.toDoLowLis = toDoLowLis;
+    }
+
+    public List<ToDoMedium> getToDoMedLis() {
+        return toDoMedLis;
+    }
+
+    public void setToDoMedLis(List<ToDoMedium> toDoMedLis) {
+        this.toDoMedLis = toDoMedLis;
+    }
+
+    public List<ToDoUrgent> getToDoUrgLis() {
+        return toDoUrgLis;
+    }
+
+    public void setToDoUrgLis(List<ToDoUrgent> toDoUrgLis) {
+        this.toDoUrgLis = toDoUrgLis;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoGroup [toDoLowLis=" + toDoLowLis + ", toDoMedLis=" + toDoMedLis + ", toDoUrgLis=" + toDoUrgLis
+                + "]";
+    }
+}
