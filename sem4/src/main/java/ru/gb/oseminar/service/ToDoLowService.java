5e1bf5321ca0e7419818e258fcfcdf45cd6ec47d
LeaClover <plesen_14@mail.ru>
2022-12-07 18:32:19 +0300

    add todo low service

---
+++ sem4/src/main/java/ru/gb/oseminar/service/ToDoLowService.java
@@ -0,0 +1,37 @@
+﻿package ru.gb.oseminar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import ru.gb.oseminar.data.ToDo;
+import ru.gb.oseminar.data.ToDoLow;
+
+public class ToDoLowService implements ToDoDataService<ToDo> {
+
+    protected List <ToDoLow> toDoLowList;
+
+    public ToDoLowService() {
+        this.toDoLowList = new ArrayList<>();
+    }
+
+    @Override
+    public void createToDo(String toDoDate, String toDoTime, String toDoDateDeadLine, 
+        String toDoTimeDeadLine, String toDoAuthor, String toDoName) {
+        Long lowID = 0L; 
+            for(ToDoLow toDo: toDoLowList) {
+                if(lowID < ((ToDoLow) toDo).getLowID()) {
+                    lowID = ((ToDoLow) toDo).getLowID();
+                }
+            }
+        ToDoLow low = new ToDoLow(++lowID, toDoDate, toDoTime, toDoDateDeadLine, toDoTimeDeadLine, toDoAuthor, toDoName);
+        this.toDoLowList.add(low);
+    }
+
+    // @Override
+    // public void deleteToDo(T param) {
+    //     super.deleteToDo(param);
+    // }
+
+    public List<ToDoLow> getLowList() {
+        return this.toDoLowList;
+    }
+}
