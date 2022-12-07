d10181a3d527355cf02c6b50e2d54a11e7f1309c
LeaClover <plesen_14@mail.ru>
2022-12-07 18:34:06 +0300

    add todo view

---
+++ sem4/src/main/java/ru/gb/oseminar/view/ToDoView.java
@@ -0,0 +1,24 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.logging.Logger;
+import ru.gb.oseminar.data.ToDo;
+
+public class ToDoView <T extends ToDo> {
+    
+    public void sendOnController(List<T> toDoList) {
+        Logger log = Logger.getAnonymousLogger();
+        log.info("\nЕще список задач!!!: \n");
+        for(T toDo: toDoList) {
+            log.info(toDo.toString());
+        }
+    }
+
+    public void sendOnControllerToDo(List<ToDo> toDoList) {
+        Logger log = Logger.getAnonymousLogger();
+        log.info("\nСписок задач: \n");
+        for(ToDo toDo: toDoList) {
+            log.info(toDo.toString());
+        }
+    }
+}
