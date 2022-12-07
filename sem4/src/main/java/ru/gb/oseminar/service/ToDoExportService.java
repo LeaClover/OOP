30a09974ad535c944a74d99aca317dc98a204001
LeaClover <plesen_14@mail.ru>
2022-12-07 18:31:34 +0300

    add todo exp service

---
+++ sem4/src/main/java/ru/gb/oseminar/service/ToDoExportService.java
@@ -0,0 +1,12 @@
+﻿package ru.gb.oseminar.service;
+
+import java.io.FileWriter;
+
+public class ToDoExportService {
+    
+    public void exportToCSV(String text) throws Exception {
+        FileWriter file = new FileWriter("C:/OOP/sem4/src/main/java/ru/gb/oseminar/text/ToDoListSAVE.csv");
+        file.write(text);
+        file.close();
+    }
+}
