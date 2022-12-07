a6ed2dd060013d281beb7bb3885d6d26bcd93b0e
LeaClover <plesen_14@mail.ru>
2022-12-07 18:30:34 +0300

    add todo low

---
+++ sem4/src/main/java/ru/gb/oseminar/data/ToDoLow.java
@@ -0,0 +1,30 @@
+﻿package ru.gb.oseminar.data;
+
+public class ToDoLow extends ToDo{
+
+    private final String statusLow = "LOW";;
+    private Long lowID;
+
+    public ToDoLow(Long lowID, String toDoDate, String toDoTime, String toDoDateDeadLine, String toDoTimeDeadLine,
+            String toDoAuthor, String toDoName) {
+        super(toDoDate, toDoTime, toDoDateDeadLine, toDoTimeDeadLine, toDoAuthor, toDoName);
+        this.lowID = lowID;
+    }
+
+    public String getStatusLow() {
+        return statusLow;
+    }
+
+    public Long getLowID() {
+        return lowID;
+    }
+
+    public void setLowID(Long lowID) {
+        this.lowID = lowID;
+    }
+
+    @Override
+    public String toString() {
+        return "ID(" + lowID + ") - " + statusLow + " - " + super.toString();
+    }
+}
