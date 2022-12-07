7fff8c8e10dc95fc8f565b1fd3779728a36494ce
LeaClover <plesen_14@mail.ru>
2022-12-07 18:31:21 +0300

    add todo dataservice

---
+++ sem4/src/main/java/ru/gb/oseminar/service/ToDoDataService.java
@@ -0,0 +1,10 @@
+﻿package ru.gb.oseminar.service;
+
+import ru.gb.oseminar.data.ToDo;
+
+public abstract interface ToDoDataService <T extends ToDo> {
+
+    void createToDo(String toDoDate, String toDoTime, String toDoDateDeadLine, String toDoTimeDeadLine,
+    String toDoAuthor, String toDoName);
+    // void deleteToDo(T param);
+}
