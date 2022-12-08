8c9016261b47e810076688976f38abc0027e0535
LeaClover <plesen_14@mail.ru>
2022-12-08 19:07:54 +0300

    add excep

---
+++ sem5/src/main/java/ru/gb/oseminar/exception/PersonalException.java
@@ -0,0 +1,14 @@
+﻿package ru.gb.oseminar.exception;
+
+public class PersonalException extends IllegalArgumentException {
+
+    public PersonalException(String message) {
+        super(message);
+    }
+
+    public PersonalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    
+}
