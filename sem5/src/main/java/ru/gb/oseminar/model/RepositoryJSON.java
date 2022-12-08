ccc369296a30449231e674b818c60973821a1059
LeaClover <plesen_14@mail.ru>
2022-12-08 19:10:06 +0300

    add rep json

---
+++ sem5/src/main/java/ru/gb/oseminar/model/RepositoryJSON.java
@@ -0,0 +1,31 @@
+﻿// package ru.gb.oseminar.model;
+
+// import java.util.List;
+
+// public class RepositoryJSON implements Repository {
+
+//     @Override
+//     public List<User> getAllUsers() {
+//         // TODO Auto-generated method stub
+//         return null;
+//     }
+
+//     @Override
+//     public String createUser(User user) {
+//         // TODO Auto-generated method stub
+//         return null;
+//     }
+
+//     @Override
+//     public void updateUser(User user) {
+//         // TODO Auto-generated method stub
+        
+//     }
+
+//     @Override
+//     public void deleteUser(String userID) {
+//         // TODO Auto-generated method stub
+        
+//     }
+    
+// }
