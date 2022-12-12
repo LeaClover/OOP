c0c8987dceb66a997e808441538f57d16d93df57
LeaClover <plesen_14@mail.ru>
2022-12-12 18:15:38 +0300

    add figure view

---
+++ sem6/src/main/java/ru/gb/oseminar/view/FigureView.java
@@ -0,0 +1,20 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.logging.Logger;
+import ru.gb.oseminar.data.Figure;
+
+public class FigureView {
+
+    public Logger log = Logger.getAnonymousLogger();
+    
+    public void sendOnConsole(List<Figure> listF) {
+        for(Figure item: listF) {
+            log.info(item.toString());
+        }
+    }
+
+    public void sendParamOnConsole(String messege, List<Double> listParam) {
+        log.info(messege + listParam.toString());
+    }
+}
