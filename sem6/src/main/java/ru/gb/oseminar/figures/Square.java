0215bdb3e9006aa4672648f6597adff6835b6d27
LeaClover <plesen_14@mail.ru>
2022-12-12 18:14:33 +0300

    add square

---
+++ sem6/src/main/java/ru/gb/oseminar/figures/Square.java
@@ -0,0 +1,34 @@
+﻿package ru.gb.oseminar.figures;
+
+public class Square extends Rectangle {
+
+    public Double side;
+
+    public Square(Double side) {
+        super(side, side);
+        this.side = side;
+    }
+
+    public Double getSide() {
+        return side;
+    }
+
+    public void setSide(Double side) {
+        this.side = side;
+    }
+
+    @Override
+    public Double area() {
+        return this.side*this.side;
+    }
+
+    @Override
+    public Double perimetr() {
+        return 4*this.side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square [side=" + side + "]";
+    }
+}
