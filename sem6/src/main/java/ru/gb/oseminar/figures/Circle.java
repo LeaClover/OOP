fa1c974f202fd194330c309ae8e070c0a1d0ad05
LeaClover <plesen_14@mail.ru>
2022-12-12 18:14:11 +0300

    add Circle

---
+++ sem6/src/main/java/ru/gb/oseminar/figures/Circle.java
@@ -0,0 +1,36 @@
+﻿package ru.gb.oseminar.figures;
+
+import ru.gb.oseminar.data.Figure;
+import ru.gb.oseminar.data.ILengthable;
+
+public class Circle extends Figure implements ILengthable {
+    private final Double pi = 3.14;
+    public Double radius;
+
+    public Circle(Double radius) {
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Double area() {
+        return this.pi*this.radius*this.radius;
+    }
+
+    @Override
+    public Double length() {
+        return 2*this.radius*this.pi;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle [radius=" + radius + "]";
+    }
+}
