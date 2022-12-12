0c3bba3e0e1d88946c5e171bd5171efb0cbc3d32
LeaClover <plesen_14@mail.ru>
2022-12-12 18:15:14 +0300

    add rep figures

---
+++ sem6/src/main/java/ru/gb/oseminar/service/RepositoryFigures.java
@@ -0,0 +1,24 @@
+﻿package ru.gb.oseminar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import ru.gb.oseminar.data.Figure;
+import ru.gb.oseminar.data.IRepository;
+
+public class RepositoryFigures implements IRepository <Figure> {
+
+    private final List<Figure> figureList;
+
+    public RepositoryFigures() {
+        this.figureList = new ArrayList<Figure>();
+    }
+
+    @Override
+    public void add(Figure value) {
+        this.figureList.add(value);
+    }
+    
+    public List<Figure> getFigureList() {
+        return this.figureList;
+    }
+}
