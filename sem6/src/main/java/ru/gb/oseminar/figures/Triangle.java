0c1b82cde297e59524958d94c6f4f781ec78c12e
LeaClover <plesen_14@mail.ru>
2022-12-12 18:14:51 +0300

    add triangle

---
+++ sem6/src/main/java/ru/gb/oseminar/figures/Triangle.java
@@ -0,0 +1,56 @@
+﻿package ru.gb.oseminar.figures;
+
+import ru.gb.oseminar.data.Poligon;
+
+public class Triangle extends Poligon {
+
+    public Double sideA;
+    public Double sideB;
+    public Double sideC;
+
+    public Triangle(Double sideA, Double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public Double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(Double sideA) {
+        this.sideA = sideA;
+    }
+
+    public Double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(Double sideB) {
+        this.sideB = sideB;
+    }
+
+    public Double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(Double sideC) {
+        this.sideC = sideC;
+    }
+
+    @Override
+    public Double area() {
+        double p = (this.sideA + this.sideB + this.sideC) / 2;
+        return Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
+    }
+
+    @Override
+    public Double perimetr() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle [sideA=" + sideA + ", sideB=" + sideB + ", sideC=" + sideC + "]";
+    }
+}
