41e0b432fcc2b41bf204a4f1d691ee255fdbd6f6
LeaClover <plesen_14@mail.ru>
2022-12-12 18:13:51 +0300

    add Poligon

---
+++ sem6/src/main/java/ru/gb/oseminar/data/Poligon.java
@@ -0,0 +1,14 @@
+﻿package ru.gb.oseminar.data;
+
+public class Poligon extends Figure implements IPerimeterable{
+
+    @Override
+    public Double area() {
+        return 0.0;
+    }
+
+    @Override
+    public Double perimetr() {
+        return 0.0;
+    }
+}
