816954a5158bfa1a3ae8c13014140a30e509d789
LeaClover <plesen_14@mail.ru>
2022-12-15 13:19:46 +0300

    add middle service

---
+++ sem7/src/main/java/ru/gb/oseminar/service/MiddleService.java
@@ -0,0 +1,21 @@
+﻿package ru.gb.oseminar.service;
+
+import ru.gb.oseminar.interfaces.ISmoke;
+import ru.gb.oseminar.interfaces.IStudy;
+
+public class MiddleService implements ISmoke, IStudy{
+
+    @Override
+    public void study() {
+        System.out.println("Я ненавижу учиться");
+    }
+
+    @Override
+    public void smoke() {
+        System.out.println("Я начинаю курить");
+    }
+    
+    public void leaveSchool() {
+        System.out.println("Я прогулял урок");
+    }
+}
