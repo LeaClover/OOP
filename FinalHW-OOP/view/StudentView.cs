b65b413bbe125b7cb07da44cbf40e4887e85a2bc
LeaClover <plesen_14@mail.ru>
2022-12-15 15:31:02 +0300

    add file student view

---
+++ FinalHW-OOP/view/StudentView.cs
@@ -0,0 +1,33 @@
+﻿namespace FinalHWOOP {
+    public class StudentView {
+        public SmallService ss = new SmallService();
+        public MiddleService ms = new MiddleService();
+        public HighService hs = new HighService();
+        
+        public void sendOnConsole(List<Student> studentList) {
+            foreach (Student student in studentList) {
+                Type type = student.getType();
+                switch(type) {
+                    case Type.HIGH:
+                        Console.WriteLine(student);
+                        hs.study();
+                        hs.smoke();
+                        hs.prepareToExam();
+                        hs.getLove();
+                        break;
+                    case Type.MIDDLE:
+                        Console.WriteLine(student);
+                        ms.study();
+                        ms.smoke();
+                        ms.leaveSchool();
+                        break;
+                    case Type.SMALL:
+                        Console.WriteLine(student);
+                        ss.study();
+                        ss.cry();
+                        break;
+                }
+            }
+        }
+    }
+}
