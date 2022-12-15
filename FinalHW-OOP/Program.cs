6bfc8fdb22c403e15c2dbbbe6db6f5d6182246c8
LeaClover <plesen_14@mail.ru>
2022-12-15 15:27:43 +0300

    add file program

---
+++ FinalHW-OOP/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalHWOOP {
+    public class Program {
+        public static void Main(string[] args) {
+            Controller cc = new Controller();
+            cc.addStudent(new Student("Lea", "Clover", "9991233445", "24.02.2003", Type.HIGH));
+            cc.addStudent(new Student("Alex", "Aleksandrov", "9859190358", "28.02.2010", Type.MIDDLE));
+            cc.addStudent(new Student("Vlad", "Fox", "9852234455", "11.07.2015", Type.SMALL));
+            cc.getAllStudents();
+        }
+    }
+}
