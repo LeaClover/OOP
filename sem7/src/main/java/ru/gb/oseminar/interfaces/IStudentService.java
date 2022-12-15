5f5751147626adc84f1cffafa783fefede3cf22f
LeaClover <plesen_14@mail.ru>
2022-12-15 13:18:31 +0300

    add I student service

---
+++ sem7/src/main/java/ru/gb/oseminar/interfaces/IStudentService.java
@@ -0,0 +1,10 @@
+﻿package ru.gb.oseminar.interfaces;
+
+import java.util.List;
+import ru.gb.oseminar.data.Student;
+
+public abstract interface IStudentService {
+    
+    public void addStudent(Student student);
+    public List<Student> getAllStudents();
+}
