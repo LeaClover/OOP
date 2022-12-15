efa782f17d8001f72873efbca547f58c14f8d2ef
LeaClover <plesen_14@mail.ru>
2022-12-15 15:30:17 +0300

    add file middle service

---
+++ FinalHW-OOP/service/MiddleService.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalHWOOP {
+    public class MiddleService : ISmoke, IStudy {
+        public void smoke() {
+            Console.WriteLine("Я начинаю курить");
+        }
+
+        public void study() {
+            Console.WriteLine("Ненавижу учиться");
+        }
+
+        public void leaveSchool() {
+            Console.WriteLine("Я прогуливаю уроки");
+        }
+    }
+}
