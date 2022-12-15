14717cf4251ca3e600c3efb0f683ead69abf9a7c
LeaClover <plesen_14@mail.ru>
2022-12-15 15:27:56 +0300

    add file controller

---
+++ FinalHW-OOP/controller/Controller.cs
@@ -0,0 +1,14 @@
+﻿namespace FinalHWOOP {
+    public class Controller {
+        private StudentService ss = new StudentService();
+        private StudentView sv = new StudentView();
+
+        public void addStudent(Student student) {
+            ss.addStudent(student);
+        }
+
+        public void getAllStudents() {
+            sv.sendOnConsole(ss.getAllStudents());
+        }
+    }
+}
