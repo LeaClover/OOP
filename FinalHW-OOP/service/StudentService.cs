f313fda14c55a66c99050ce89d12bbf795b64aba
LeaClover <plesen_14@mail.ru>
2022-12-15 15:30:46 +0300

    add file student service

---
+++ FinalHW-OOP/service/StudentService.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalHWOOP {
+    public class StudentService : IStudentService {
+        public List<Student> studentList;
+
+        public StudentService() {
+            this.studentList = new List<Student>{};
+        }
+
+        public void addStudent(Student student) {
+            this.studentList.Add(student);
+        }
+
+        public List<Student> getAllStudents() {
+            return this.studentList;
+        }
+    }
+}
