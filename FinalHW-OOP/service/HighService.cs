cb4e81c04571bbb2b2a086cb8fe7c07d7a97fb99
LeaClover <plesen_14@mail.ru>
2022-12-15 15:30:00 +0300

    add file high service

---
+++ FinalHW-OOP/service/HighService.cs
@@ -0,0 +1,19 @@
+﻿namespace FinalHWOOP {
+    public class HighService : ISmoke, IStudy {
+        public void smoke() {
+            Console.WriteLine("Я бросаю курить");
+        }
+
+        public void study() {
+            Console.WriteLine("Мне надо учиться");
+        }
+
+        public void getLove() {
+            Console.WriteLine("У меня есть отношения");
+        } 
+
+        public void prepareToExam() {
+            Console.WriteLine("Я готовлюсь к экзаменам");
+        }
+    }
+}
