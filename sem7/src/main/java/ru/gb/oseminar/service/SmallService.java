8ce037cf7e2f90bf083354194a4be18f79c44277
LeaClover <plesen_14@mail.ru>
2022-12-15 13:19:22 +0300

    add small service

---
+++ sem7/src/main/java/ru/gb/oseminar/service/SmallService.java
@@ -0,0 +1,15 @@
+﻿package ru.gb.oseminar.service;
+
+import ru.gb.oseminar.interfaces.IStudy;
+
+public class SmallService implements IStudy {
+
+    @Override
+    public void study() {
+        System.out.println("Я люблю учиться");
+    }
+    
+    public void cry() {
+        System.out.println("Хочу домой к маме!");
+    }
+}
