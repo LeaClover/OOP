f96876a5789d7ebe4d5e5b002ff1d8bffaea851a
LeaClover <plesen_14@mail.ru>
2022-12-15 13:17:29 +0300

    add controller

---
+++ sem7/src/main/java/ru/gb/oseminar/controller/Controller.java
@@ -0,0 +1,19 @@
+﻿package ru.gb.oseminar.controller;
+
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.service.StudentService;
+import ru.gb.oseminar.view.StudentView;
+
+public class Controller {
+
+    private StudentService ss = new StudentService();
+    private StudentView sv = new StudentView();
+
+    public void addStudent(Student student) {
+        ss.addStudent(student);
+    }
+
+    public void getAllStudents() {
+        sv.sendOnConsole(ss.getAllStudents());
+    }   
+}
