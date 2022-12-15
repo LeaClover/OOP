1e14a14c32c21c4f4c9d92442e2a9700bee8a541
LeaClover <plesen_14@mail.ru>
2022-12-15 13:19:34 +0300

    add high service

---
+++ sem7/src/main/java/ru/gb/oseminar/service/HighService.java
@@ -0,0 +1,25 @@
+﻿package ru.gb.oseminar.service;
+
+import ru.gb.oseminar.interfaces.ISmoke;
+import ru.gb.oseminar.interfaces.IStudy;
+
+public class HighService implements ISmoke, IStudy{
+
+    @Override
+    public void study() {
+        System.out.println("Я должен учиться");
+    }
+
+    @Override
+    public void smoke() {
+        System.out.println("Я бросаю курить");
+    }
+    
+    public void prepareToExam() {
+        System.out.println("Я готовлюсь к экзаменам");
+    }
+
+    public void getLove() {
+        System.out.println("У меня отношения!");
+    }
+}
