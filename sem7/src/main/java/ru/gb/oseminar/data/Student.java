db6dabc78ea2970d8d95a96c7a8df25d460cc7c7
LeaClover <plesen_14@mail.ru>
2022-12-15 13:17:51 +0300

    add student

---
+++ sem7/src/main/java/ru/gb/oseminar/data/Student.java
@@ -0,0 +1,64 @@
+﻿package ru.gb.oseminar.data;
+
+public class Student {
+
+    public String firstName;
+    public String lastName;
+    public String phone;
+    public String dateOfBirth;
+    public StudentType type;
+
+    public Student(String firstName, String lastName, String phone, String dateOfBirth, StudentType type) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.dateOfBirth = dateOfBirth;
+        this.type = type;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public StudentType getType() {
+        return type;
+    }
+
+    public void setType(StudentType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone + ", dateOfBirth="
+                + dateOfBirth + ", type=" + type + "]";
+    }
+}
