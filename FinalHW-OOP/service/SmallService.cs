bfcc6a2c164f7d4e98f452d521d48ddc502d7954
LeaClover <plesen_14@mail.ru>
2022-12-15 15:30:29 +0300

    add file small service

---
+++ FinalHW-OOP/service/SmallService.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalHWOOP {
+    public class SmallService : IStudy {
+
+        public void study() {
+            Console.WriteLine("Я люблю учиться");
+        }
+
+        public void cry() {
+            Console.WriteLine("Хочу домой к маме!");
+        }
+    }
+}
