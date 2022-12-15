3e9d6bc721655d2c55d10b259aea29b16fd642ea
LeaClover <plesen_14@mail.ru>
2022-12-15 13:20:20 +0300

    add student view

---
+++ sem7/src/main/java/ru/gb/oseminar/view/StudentView.java
@@ -0,0 +1,43 @@
+﻿package ru.gb.oseminar.view;
+
+import java.util.List;
+import java.util.logging.Logger;
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.data.StudentType;
+import ru.gb.oseminar.service.HighService;
+import ru.gb.oseminar.service.MiddleService;
+import ru.gb.oseminar.service.SmallService;
+
+public class StudentView {
+
+    private HighService hs = new HighService();
+    private MiddleService ms = new MiddleService();
+    private SmallService ss = new SmallService();
+
+    public void sendOnConsole(List<Student> studentList) {
+        Logger log = Logger.getAnonymousLogger();
+        for(Student student: studentList) {
+            StudentType type = student.getType();
+            switch (type) {
+                case HIGH:
+                    log.info(student.toString());
+                    hs.study();;
+                    hs.smoke();
+                    hs.prepareToExam();
+                    hs.getLove();
+                    break;
+                case MIDDLE:
+                    log.info(student.toString());
+                    ms.study();;
+                    ms.smoke();
+                    ms.leaveSchool();
+                    break;
+                case SMALL:
+                    log.info(student.toString());
+                    ss.study();
+                    ss.cry();
+                    break;
+            }
+        }
+    }  
+}
