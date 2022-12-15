44fe04c1b86038dfb187f1bc4ce86264aa2425d5
LeaClover <plesen_14@mail.ru>
2022-12-15 13:20:01 +0300

    add student service

---
+++ sem7/src/main/java/ru/gb/oseminar/service/StudentService.java
@@ -0,0 +1,26 @@
+﻿package ru.gb.oseminar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import ru.gb.oseminar.data.Student;
+import ru.gb.oseminar.interfaces.IStudentService;
+
+public class StudentService implements IStudentService {
+
+    private List<Student> studentList;
+    
+    public StudentService() {
+        this.studentList = new ArrayList<>();
+    }
+
+    @Override
+    public void addStudent(Student student) {
+        this.studentList.add(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return this.studentList;
+    }
+    
+}
